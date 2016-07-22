8f5214bc678d5604c0f5ea7b5aa7a4c44a144c39
platy11 <mightymaxmax@gmail.com>
2016-07-22 20:31:39 +1000

    total updates

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
-var app = require('express')();
+var app = require('express')()
 var express3 = require('express3')
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
-app.use(express3.basicAuth(process.env.USER || 'trade', process.env.PASS || 'mark'));
+var Colours = [], Users = [];
+
+app.use(express3.basicAuth(process.env.USER || 'trade', process.env.PASS || 'mark'))
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+  res.sendFile(__dirname + '/index.html')
+})
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('a user connected')
+  UserCheck()
+  io.emit('chat message', '<strong style="color:red">A user connected</strong>')
   socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+    UserCheck()
+    console.log('user disconnected')
+    io.emit('chat message', '<strong style="color:blue">A user disconnected</strong>')
+  })
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-});
+    if (msg.message == '') {
+      return
+    }
+    if (!Colours[msg.user]) {
+      var col = '#'+Math.floor(Math.random()*16777215).toString(16)
+      Colours[msg.user] = col
+    }
+    msg.message = msg.message.replace(/script/igm, 'scrip_t').replace('on', 'o_n')
+    console.log('message: ' + msg.message + '(' + msg.user + ')')
+    io.emit('chat message', '<strong style="color:' + Colours[msg.user] + '">' + msg.user + '</strong> ' + msg.message)
+  })
+  socket.on('user active', function (msg) {
+    Users.push('<span>' + msg + '</span>')
+  })
+})
+
+function UserCheck() {
+  Users = []
+  // console.log('Checking users');
+  io.emit('check users', '')
+  setTimeout(function () {
+    // console.log('Updating users');
+    io.emit('users update', Users)
+  }, 2000)
+}
 
 http.listen(process.env.PORT || 80, function(){
-  console.log('listening on *:3000');
-});
+  console.log('listening on *:3000')
+})
