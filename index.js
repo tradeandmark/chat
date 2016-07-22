a994bbf07d1f75d12f5dda6981534e8f41701b29
platy11 <mightymaxmax@gmail.com>
2016-07-22 17:49:09 +1000

    fix

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  // console.log('a user connected');
-  // socket.on('disconnect', function(){
-  //   console.log('user disconnected');
-  // });
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
