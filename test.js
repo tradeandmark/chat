51ccf861edc688b5b728b9b97b99e6a792be4bba
platy11 <mightymaxmax@gmail.com>
2016-07-28 17:36:24 +1000

    Add test

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
+var request = require('request')
 var index = require('./index')
 
-setTimeout(function(){
-  process.exit()
-}, 10 * 1000)
+request('http://localhost:3238/', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var file = fs.readFileSync("index.html", "utf8");
+    if (file === body) {
+      console.log('All good: We could read /')
+      process.exit(0)
+    } else {
+      console.log('ERR: We did not get what we were expecting')
+      process.exit(1)
+    }
+  } else {
+    console.log('ERR: Could not get /')
+    process.exit(1)
+  }
+})
