8b829eb514f004c258ce24f79b15263fcfc50ff7
platy11 <mightymaxmax@gmail.com>
2016-07-28 17:47:47 +1000

    Add CSS test

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ request('http://localhost:3238/', function (error, response, body) {
     var file = fs.readFileSync("index.html", "utf8");
     if (file === body) {
       console.log('All good: We could read /')
-      process.exit(0)
+      getCss()
     } else {
       console.log('ERR: We did not get what we were expecting')
       process.exit(1)
@@ -17,3 +17,21 @@ request('http://localhost:3238/', function (error, response, body) {
     process.exit(1)
   }
 })
+
+function getCss() {
+  request('http://localhost:3238/styles.css', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var file = fs.readFileSync("styles.css", "utf8");
+      if (file === body) {
+        console.log('All good: We could read /styles.css')
+        process.exit(0)
+      } else {
+        console.log('ERR: We did not get what we were expecting')
+        process.exit(1)
+      }
+    } else {
+      console.log('ERR: Could not get /styles.css')
+      process.exit(1)
+    }
+  })
+}
